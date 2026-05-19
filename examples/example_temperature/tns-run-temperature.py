c80bf5a82df65c5b52036cabb0069e3720fe846d
Ana Stuhec <ana.stuhec@hotmail.com>
2026-05-19 08:36:53 +0200

    added dir

--- examples/example_temperature/tns-run-temperature.py
+++ examples/example_temperature/tns-run-temperature.py
@@ -6,27 +6,34 @@ import matplotlib.pyplot as plt
 
 ####################################################################################################################
 
-''' Add path to programs '''
-sys.path.insert(0, '/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/main-programs/')
+''' Add path to programs and parameters '''
+computer = 'ana' # if on mac, else 'anast' if on lenovo
+
+if computer == 'anast':
+    DIR = '/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/'
+elif computer == 'ana':
+    DIR = '/Users/ana/Desktop/tns-ana/'
+
+print('Running on computer: ' + DIR)
+sys.path.insert(0, DIR + 'main-programs/')
 import tns_module as module
 import tns_helpers as helpers
 import tns_tokovi as tokovi
 
-''' Add path to parameters '''
-hopping_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/main-programs/parameters-kinetic.txt"
-interaction_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/main-programs/parameters-interaction.txt"
-perturbation_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/main-programs/parameters-perturbation.txt"
+hopping_file = DIR + 'main-programs/parameters-kinetic.txt'
+interaction_file = DIR + 'main-programs/parameters-interaction.txt'
+perturbation_file = DIR + 'main-programs/parameters-perturbation.txt'
 
 ''' Add path to input files:
     - input.json: general parameters
     - input_temperature.json: temperatures at which to evaluate transport, spectral width Gamma
     '''
-input_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/input.json"
-temperature_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/input_temperature.json"
+input_file = DIR + 'examples/input.json'
+temperature_file = DIR + 'examples/input_temperature.json'
 
 ''' Add path to output file
 results will be saved already during the run, in case the run is not completed '''
-file_output = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/transport_DC_results.npz"
+file_output = DIR + 'examples/transport_DC_results.npz'
 
 ####################################################################################################################
 
