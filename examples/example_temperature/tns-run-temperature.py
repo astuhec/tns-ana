af83ed0e714f129b1cdad018e593ac6d6078525e
Ana <ana.stuhec44@gmail.com>
2026-05-19 08:33:21 +0200

    fixed path

--- examples/example_temperature/tns-run-temperature.py
+++ examples/example_temperature/tns-run-temperature.py
@@ -21,12 +21,12 @@ perturbation_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/main-program
     - input.json: general parameters
     - input_temperature.json: temperatures at which to evaluate transport, spectral width Gamma
     '''
-input_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/input.json"
-temperature_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/input_temperature.json"
+input_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/example_temperature/input.json"
+temperature_file = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/example_temperature/input_temperature.json"
 
 ''' Add path to output file
 results will be saved already during the run, in case the run is not completed '''
-file_output = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/transport_DC_results.npz"
+file_output = "/Users/anast/OneDrive/Namizje/tns-repo/tns-ana/examples/example_temperature/transport_DC_results.npz"
 
 ####################################################################################################################
 
