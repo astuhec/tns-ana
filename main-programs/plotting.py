23c495db2bed803e14d170936b64005dc424c53d
Ana Stuhec <ana.stuhec@hotmail.com>
2026-06-22 11:46:31 +0200

    plotting bands,optics

--- main-programs/plotting.py
+++ main-programs/plotting.py
@@ -28,6 +28,8 @@ def bands(energije, barve, mu, name='bands', a=3.51, b=15.79, col1='firebrick', 
         E = energije[i, Ny//2, Nx//2:]
         for g in range(len(K)-1):
             shade = np.mean(barve[i, Ny//2, Nx//2:][g:g+2])
+            if shade > 1.0:
+                shade = 1.0
             plt.plot(K[g:g+2], E[g:g+2]- mu, color=colorFader(col1, col2, shade))
 
     # Z - Gamma
@@ -38,6 +40,8 @@ def bands(energije, barve, mu, name='bands', a=3.51, b=15.79, col1='firebrick', 
         E = energije[i, :Ny//2, Nx//2]
         for g in range(len(K)-1):
             shade = np.mean(barve[i, :Ny//2, Nx//2][g:g+2])
+            if shade > 1.0:
+                shade = 1.0
             plt.plot(K[g:g+2], E[g:g+2]- mu, color=colorFader(col1, col2, shade))
 
 
@@ -47,6 +51,9 @@ def bands(energije, barve, mu, name='bands', a=3.51, b=15.79, col1='firebrick', 
         E = energije[i, 0, Nx//2:][::-1]
         for g in range(len(K)-1):
             shade = np.mean(barve[i, 0, Nx//2:][::-1][g:g+2])
+            print(shade)
+            if shade > 1.0:
+                shade = 1.0
             plt.plot(K[g:g+2], E[g:g+2] - mu, color=colorFader(col1, col2, shade))
 
     ax.set_xticks([0, (Nx//2 -1)*kX, (Nx//2 -1)*kX + (Ny//2 - 1)*kY, (Nx//2 -1)*kX + (Ny//2 - 1)*kY + (Nx//2-1)*kX],)
@@ -56,5 +63,44 @@ def bands(energije, barve, mu, name='bands', a=3.51, b=15.79, col1='firebrick', 
     plt.ylabel(r'$\varepsilon_{\bm{k}} - E_F\,[\text{eV}]$', fontsize=18)
 
     plt.title(r'band structure along $M$--$Z$--$\Gamma$--$X$', fontsize=16)
+    plt.tight_layout()
+    plt.savefig(f'{name}.pdf')
+    
+def optics(omega0, results_x, results_y, results_xy, results_yx, name='optics'):
+    fig, ax = plt.subplots(ncols=2, nrows=2, figsize=(8,8))
+
+    # direction x (axis a)
+    sigma_x = -results_x['chi_jj0'].imag / omega0
+    dsigma_x = -results_x['dchi_jj'].imag / omega0
+    ax[0,0].plot(omega0, sigma_x, label='without vertex corrections')
+    ax[0,0].plot(omega0, sigma_x + dsigma_x, label='with vertex corrections')
+    ax[0,0].set_ylabel(r'$\sigma_x$')
+
+    # direction y (axis c)
+    sigma_y = -results_y['chi_jj0'].imag / omega0
+    dsigma_y = -results_y['dchi_jj'].imag / omega0
+    ax[0,1].plot(omega0, sigma_y, label='without vertex corrections')
+    ax[0,1].plot(omega0, sigma_y + dsigma_y, label='with vertex corrections')
+    ax[0,1].set_ylabel(r'$\sigma_y$') 
+
+    # mixed xy
+    sigma_xy = -results_xy['chi_jj0'].imag / omega0
+    dsigma_xy = -results_xy['dchi_jj'].imag / omega0
+    ax[1,0].plot(omega0, sigma_xy, label='without vertex corrections')
+    ax[1,0].plot(omega0, sigma_xy + dsigma_xy, label='with vertex corrections')
+    ax[1,0].set_ylabel(r'$\sigma_{xy}$') 
+
+    # mixed yx
+    sigma_yx = -results_yx['chi_jj0'].imag / omega0
+    dsigma_yx = -results_yx['dchi_jj'].imag / omega0
+    ax[1,1].plot(omega0, sigma_yx, label='without vertex corrections')
+    ax[1,1].plot(omega0, sigma_yx + dsigma_yx, label='with vertex corrections')
+    ax[1,1].set_ylabel(r'$\sigma_{yx}$')
+
+    for j in range(2):
+        for i in range(2):
+            ax[j,i].set_xlabel(r'$\omega\,(\text{eV})$')
+            ax[j,i].legend()
+
     plt.tight_layout()
     plt.savefig(f'{name}.pdf')
